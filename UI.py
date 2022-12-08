d21bc4862f110e85cdf2cfcf32dd200f97490b76
EgijaG <egija.graudina@gmail.com>
2022-12-08 18:38:59 +0200

    Initial comments and window for GUI

---
+++ UI.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+window = tk.Tk()
+
+# Creating text for user information
+greeting = tk.Label(text="Hello, dear user. This is a program where you will be able to encrypt & decrypt your text files.")
+info = tk.Label(text="You should choose the process - ENCRYPT or DECRYPT and encryption standard.")
+# adding text to the window
+greeting.pack()
+info.pack()
+
+# Setting window size
+window.geometry("800x600")
+
+# Choice of process - encryption or decryption
+
+# Creating buttons for different choices and method calling
+
+# Implementing file browser
+
+# Implementing key input fields
+
+
+
+
+
+window.mainloop()
